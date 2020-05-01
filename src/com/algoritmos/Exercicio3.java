f4fde697459945d9d1a29f6ab4d19d6f66c4fb6d
jessica.manoel <jessica.manoel@acad.pucrs.br>
2020-05-01 20:12:43 -0300

    Exercício 3 Co-authored-by: Bianca <bianca.camargo@acad.pucrs.br>

--- src/com/algoritmos/Exercicio3.java
+++ src/com/algoritmos/Exercicio3.java
@@ -6,7 +6,21 @@ import java.util.Random;
 import static java.lang.Integer.max;
 
 /*
-* */
+* O algoritmo a seguir (que utiliza divisão-e-conquista) encontra o maior valor em um vetor.
+* Assim, novamente:
+* implemente o algortimo abaixo;
+* teste-o para vetores de inteiros com conteúdos randômicos, e tamanho 32, 2048 e 1.048.576.
+* Nestes testes, contabilize o número de iterações que o algoritmo executa, e o tempo gasto;
+*           int maxVal2(int A[], int init, int end) {
+*               if (end - init <= 1)
+*                   return max(A[init], A[end]);
+*               else {
+*                   int m = (init + end)/2;
+*                   int v1 = maxVal2(A,init,m);
+*                   int v2 = maxVal2(A,m+1,end);
+*                   return max(v1,v2);
+*               }
+}*/
 public class Exercicio3 {
     private static int iteracoes;
 
@@ -24,7 +38,7 @@ public class Exercicio3 {
 
     public static void main(String[] args) {
         Random random = new Random();
-        final int SIZE = 1000000;
+        final int SIZE = 32;
         int[] unsortedArray = new int[SIZE];
         for (int i = 0; i < SIZE; i++) {
             unsortedArray[i] = random.nextInt(100);
